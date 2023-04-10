a23b06049358f532b3a5e0af3e657c2c76c39f75
Igor Tron <public@itron.pro>
2023-04-10 21:04:49 +0200

    vendor forwarding

--- install/sync.sh
+++ install/sync.sh
@@ -13,7 +13,9 @@ echo -e "${BBLACK}"
 echo "Synchronizing working directory"
 
 [ ! -d ./wordpress/wp-content/plugins/neuralseo ] && mkdir -p ./wordpress/wp-content/plugins/neuralseo
+[ ! -d ./wordpress/wp-content/plugins/neuralseo/vendor ] && mkdir -p ./wordpress/wp-content/plugins/neuralseo/vendor
 rsync -cav"${PARAM}" --delete ./neuralseo/app/ ./wordpress/wp-content/plugins/neuralseo/
+rsync -cav"${PARAM}" --delete ./neuralseo/vendor/ ./wordpress/wp-content/plugins/neuralseo/vendor/
 
 echo "Done"
 echo -e "${COLOR_OFF}"
