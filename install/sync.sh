3495ba8959a3b995b844303a921c256a53d7461b
Igor Tron <public@itron.pro>
2023-04-13 18:30:37 +0200

    app folder got removed

--- install/sync.sh
+++ install/sync.sh
@@ -13,9 +13,7 @@ echo -e "${BBLACK}"
 echo "Synchronizing working directory"
 
 [ ! -d ./wordpress/wp-content/plugins/neuralseo ] && mkdir -p ./wordpress/wp-content/plugins/neuralseo
-[ ! -d ./wordpress/wp-content/plugins/neuralseo/vendor ] && mkdir -p ./wordpress/wp-content/plugins/neuralseo/vendor
-rsync -cav"${PARAM}" --delete ./neuralseo/app/ ./wordpress/wp-content/plugins/neuralseo/
-rsync -cav"${PARAM}" --delete ./neuralseo/vendor/ ./wordpress/wp-content/plugins/neuralseo/vendor/
+rsync -av"${PARAM}" --delete ./neuralseo/ ./wordpress/wp-content/plugins/neuralseo/
 
 echo "Done"
 echo -e "${COLOR_OFF}"
